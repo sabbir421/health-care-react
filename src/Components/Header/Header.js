63eb154b4a5ef2fa66ab443c1ae6cee4d6994b67
sabbir421 <sabbiralazim421@gmail.com>
2021-10-18 13:17:13 +0600

    set auth

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,32 +6,27 @@ import useAuth from '../../Hooks/useAuth';
 
 
 const Header = () => {
-  const{user}=useAuth;
+  const{user,logOut}=useAuth();
+  console.log(user);
     return (
         <>
-  <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg">
-    <Container>
-    <Navbar.Brand href="#home" className='ms-5 text-primary'>SAS Medical</Navbar.Brand>
-    <Navbar.Toggle /> 
-    <Navbar.Collapse className="justify-content-end">
-   
-    
-      <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
-      <Nav.Link as={Link} to="/services#services">Services</Nav.Link>
-      <Nav.Link as={Link} to="#pricing">About Us</Nav.Link>
-      { user?.email ?  <Button variant='light'>Log Out</Button> :
-       
-        <Nav.Link as={Link} to='/login'>Log in</Nav.Link>
-        }
-      
-      
-      <Navbar.Text className='ms-3'>
-      sing in as  <a href="#login">{user?.displayName }</a>
-      </Navbar.Text>
-    </Navbar.Collapse>
-    
-    </Container>
-  </Navbar>
+         <Navbar bg="dark" variant="dark" sticky="top" collapseOnSelect expand="lg" >
+                <Container>
+                    <Navbar.Brand href="#home">Genius</Navbar.Brand>
+                    <Navbar.Toggle />
+                    <Navbar.Collapse className="justify-content-end">
+                        <Nav.Link as={Link} to="/home#home">Home</Nav.Link>
+                        <Nav.Link as={Link} to="/home#services">Services</Nav.Link>
+                        <Nav.Link as={Link} to="/home#experts">Experts</Nav.Link>
+                        {user?.email ?
+                            <Button onClick={logOut} variant="light">Logout</Button> :
+                            <Nav.Link as={Link} to="/login">Login</Nav.Link>}
+                        <Navbar.Text>
+                            Signed in as: <a href="#login">{user?.displayName}</a>
+                        </Navbar.Text>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
   
 </>
     );
--- src/Hooks/useFirebase.js
+++ src/Hooks/useFirebase.js
@@ -1,53 +1,50 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider, signOut, onAuthStateChanged } from "firebase/auth";
-import { useState, useEffect } from 'react';
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
+import { useState, useEffect } from "react";
 import initAuth from "../Firebase/FirebaseInit";
 
-
-initAuth()
+initAuth();
 
 const useFirebase = () => {
-    const [user, setUser] = useState({});
-    const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState({});
 
-    const auth = getAuth();
+  const auth = getAuth();
 
-    const signInUsingGoogle = () => {
-        setIsLoading(true);
-        const googleProvider = new GoogleAuthProvider();
+  const signInUsingGoogle = () => {
+    const googleProvider = new GoogleAuthProvider();
 
-        signInWithPopup(auth, googleProvider)
-            .then(result => {
-                setUser(result.user);
-            })
-            .finally(() => setIsLoading(false));
-    }
+    signInWithPopup(auth, googleProvider).then((result) => {
+      console.log(user);
+      setUser(result.user);
+    });
+  };
 
-    // observe user state change
-    useEffect(() => {
-        const unsubscribed = onAuthStateChanged(auth, user => {
-            if (user) {
-                setUser(user);
-            }
-            else {
-                setUser({})
-            }
-            setIsLoading(false);
-        });
-        return () => unsubscribed;
-    }, [])
+  // observe user state change
+  useEffect(() => {
+    const unsubscribed = onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setUser(user);
+      } else {
+        setUser({});
+      }
+    });
+    return () => unsubscribed;
+  }, []);
 
-    const logOut = () => {
-        setIsLoading(true);
-        signOut(auth)
-            .then(() => { })
-            .finally(() => setIsLoading(false));
-    }
+  const logOut = () => {
+    signOut(auth).then(() => {});
+  };
 
-    return {
-        user,
-        signInUsingGoogle,
-        logOut
-    }
-}
+  return {
+    user,
+    signInUsingGoogle,
+    logOut,
+  };
+};
 
 export default useFirebase;
